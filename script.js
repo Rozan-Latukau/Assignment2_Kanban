d8f8e6839943ef47ebf9d3ae1eb47c6e5f56d71e
Rozan-Latukau <iyanlatu@gmail.com>
2023-09-20 11:26:59 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,66 @@
+function searchNew(e) {
+    e.preventDefault()
+    const nilai = document.getElementById("inputsearch").value;
+    const hasil = document.createElement("div");
+
+    hasil.innerText = nilai;
+    hasil.style.backgroundColor = '#94A684'
+    hasil.style.color = 'white';
+    hasil.style.borderRadius = '5px'
+    hasil.style.padding = '10px'
+    hasil.style.marginTop = '15px';
+    // hasil.innerHTML = `<h3>${nilai}</h3><form onsubmit="submitNew(event)"><button>Next</button></form>`;
+
+    hasil.draggable = true;
+    hasil.id = "hasil-dragdrop"
+
+    hasil.ondragstart = dragStart;
+
+    document.getElementById("review").appendChild(hasil);
+
+    document.getElementById("inputsearch").value = "";
+}
+
+
+function dragStart(e) {
+    e.dataTransfer.setData("text", e.target.id);
+    console.log(e);
+}
+
+function allowDrop(e) {
+    e.preventDefault();
+    console.log(e);
+}
+
+function dropStart(e) {
+    e.preventDefault();
+    const data = e.dataTransfer.getData("text");
+    const target = e.target;
+
+    if (target.classList.contains("box")) {
+        const dragdrop = document.getElementById("review");
+        dragdrop.appendChild(document.getElementById(data));
+    }
+}
+
+function drop(e) {
+    e.preventDefault();
+    const data1 = e.dataTransfer.getData("text");
+    const target = e.target;
+
+    if (target.classList.contains("box")) {
+        const dragdrop = document.getElementById("progress");
+        dragdrop.appendChild(document.getElementById(data1));
+    }
+}
+
+function dropToDone(e) {
+    e.preventDefault();
+    const data2 = e.dataTransfer.getData("text");
+    const target = e.target;
+
+    if (target.classList.contains("box")) {
+        const dragdrop = document.getElementById("done");
+        dragdrop.appendChild(document.getElementById(data2));
+    }
+}
